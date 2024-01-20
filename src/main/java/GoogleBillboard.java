af9fad4bf8f174ca8a59cce8815dd9ee9d1f43b3
nat3chan <142937438+nat3chan@users.noreply.github.com>
2024-01-19 17:30:06 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -5,7 +5,7 @@ public class GoogleBillboard {
   String digits = e.substring(i,i+10);
   double dNum = Double.parseDouble(digits);
   if(isPrime(dNum)==true){
-      System.out.println(dNum);
+      args[i] = digits;
   }
 }
 }
